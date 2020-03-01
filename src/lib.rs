0ac624bdd7199e4828e861cbf8303975b3896b8f
yoshihara <594134+yoshihara@users.noreply.github.com>
2020-03-01 16:59:43 +0900

    make find_files() as case-insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,7 @@ fn find_files_in_directory(query: &str, dir: &str) {
                 } else if path.is_dir() {
                     find_files_in_directory(query, path_str)
                 } else {
-                    // TODO
-                    if path_str.contains(query) {
+                    if path_str.to_ascii_lowercase().contains(query) {
                         println!("found {}!", path_str);
                    }
                 }
@@ -36,7 +35,7 @@ pub struct Config {
 impl Config {
     pub fn new(args: &Vec<String>) -> Result<Self, &'static str> {
         if args.len() > 2 {
-            Ok(Config { query: (&args[1]).to_string(), path: (&args[2]).to_string() })
+            Ok(Config { query: (&args[1]).to_ascii_lowercase().to_string(), path: (&args[2]).to_string() })
         } else {
             Err("not enough argument")
         }
