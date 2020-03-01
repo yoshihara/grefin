57b0e0339a1c22ff0ecfc900a4788bd6e209e427
yoshihara <594134+yoshihara@users.noreply.github.com>
2020-03-01 16:44:08 +0900

    Implement find_files (find dir -name name)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
+use std::fs;
+
 pub fn run(config: Config) {
     find_files(config);
 }
 
 fn find_files(config: Config) {
-    println!("{} in {}", config.query, config.path);
+    find_files_in_directory(&config.query, &config.path);
 }
 
+fn find_files_in_directory(query: &str, dir: &str) {
+    for entry in fs::read_dir(dir).expect("Failed to read directory") {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if let Some(x) = path.to_str() {
+                if path.is_dir() {
+                    find_files_in_directory(query, x)
+                } else {
+                    // TODO
+                    if x.contains(query) {
+                        println!("found {}!", x);
+                   }
+                }
+            }
+        }
+    }
+}
 #[derive(Debug)]
 pub struct Config {
     pub query: String,
