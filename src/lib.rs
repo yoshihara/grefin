c84bdf15a24af78b9828d3703e7381b190ae5ced
yoshihara <594134+yoshihara@users.noreply.github.com>
2020-03-01 17:48:30 +0900

    Add tests for find_files_in_directory()

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 use std::fs;
+use std::io::{self, Write};
 
 pub fn run(config: Config) {
     find_files(config);
 }
 
 fn find_files(config: Config) {
-    find_files_in_directory(&config.query, &config.path);
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+
+    find_files_in_directory(&mut stdout, &config.query, &config.path);
 }
 
-fn find_files_in_directory(query: &str, dir: &str) {
+fn find_files_in_directory<W: Write>(w: &mut W, query: &str, dir: &str) {
     for entry in fs::read_dir(dir).expect("Failed to read directory") {
         if let Ok(entry) = entry {
             let path = entry.path();
@@ -16,10 +20,10 @@ fn find_files_in_directory(query: &str, dir: &str) {
                 if path_str.ends_with(".git") {
                     continue;
                 } else if path.is_dir() {
-                    find_files_in_directory(query, path_str)
+                    find_files_in_directory(w, query, path_str)
                 } else {
                     if path_str.to_ascii_lowercase().contains(query) {
-                        println!("found {}!", path_str);
+                        writeln!(w, "filename: {}", path_str).expect("output error");
                    }
                 }
             }
@@ -60,4 +64,37 @@ mod test_config {
 
         assert!(Config::new(&args).is_err());
     }
+}
+
+#[test]
+fn test_find_files_in_directory_with_found() {
+    let mut output_stub = Vec::<u8>::new();
+    find_files_in_directory(&mut output_stub, "hoge", "fixtures/");
+
+    assert_eq!(String::from_utf8(output_stub).unwrap(), "filename: fixtures/test/foo/hoge.txt\n");
+}
+
+#[test]
+fn test_find_files_in_directory_without_found() {
+    let mut output_stub = Vec::<u8>::new();
+    find_files_in_directory(&mut output_stub, "notfound", "fixtures/");
+
+    assert_eq!(String::from_utf8(output_stub).unwrap(), "");
+}
+
+#[test]
+fn test_find_files_in_directory_with_found_upcase() {
+    let mut output_stub = Vec::<u8>::new();
+    find_files_in_directory(&mut output_stub, "fuga", "fixtures/");
+
+    assert_eq!(String::from_utf8(output_stub).unwrap(), "filename: fixtures/test/foo/FUGA.txt\n");
+}
+
+
+#[test]
+fn test_find_files_in_directory_with_found_dir() {
+    let mut output_stub = Vec::<u8>::new();
+    find_files_in_directory(&mut output_stub, "foo", "fixtures/");
+
+    assert_eq!(String::from_utf8(output_stub).unwrap(), "filename: fixtures/test/foo/FUGA.txt\nfilename: fixtures/test/foo/hoge.txt\n");
 }
