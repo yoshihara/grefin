c174bff8efcc8e6ac75b32300d5ff1999432bee0
yoshihara <594134+yoshihara@users.noreply.github.com>
2020-03-29 21:56:27 +0900

    [WIP] Implement grep_files()
    TODO: tests

--- src/grep_files.rs
+++ src/grep_files.rs
@@ -1,5 +1,45 @@
-use super::Config;
+use std::fs::File;
+use std::io::Read;
+use super::*;
 
-pub fn grep_files(_config: &Config) -> Result<(), String> {
+pub fn grep_files(config: &Config) -> Result<(), String> {
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+
+    grep_files_in_directory(&mut stdout, &config.query, &config.path)
+}
+
+fn grep_files_in_directory<W: Write>(w: &mut W, query: &str, dir: &str) -> Result<(), String>{
+    let entries = fs::read_dir(dir).map_err(|e| format!("{}: {}", dir, e))?;
+
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+
+            if let Some(filename) = path.file_name() {
+                let filename_str = filename.to_str().ok_or("converting path error".to_string())?;
+
+                if filename_str == ".git" {
+                    continue;
+                } else if path.is_dir() {
+                    if let Err(err) = grep_files_in_directory(w, query, path.to_str().ok_or("converting path error".to_string())?) {
+                        return Err(err);
+                    }
+                } else {
+                    let mut f = File::open(path.as_os_str()).expect("file not found");
+                    let mut contents = String::new();
+                    let mut count = 0;
+                    f.read_to_string(&mut contents)
+                        .expect("something went wrong reading the file");
+                    for line in contents.lines() {
+                        count += 1;
+                        if line.contains(query) {
+                            writeln!(w, "{}:{}: {}", path.to_str().unwrap(), count, line).expect("output error");
+                        }
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
